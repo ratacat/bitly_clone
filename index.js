78cf0a7eadb01c02fad5ff10c8dfbe322fedda97
ratacat <neuralsplash@gmail.com>
2015-05-12 17:07:11 -0700

    input view up

---
+++ index.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var app = express();
+
+//make form input 
+//receive post input for a bitly form
+//add data about redirector to array
+//process incoming requests as redirects
